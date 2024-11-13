6c7a46ab756699527d1cd426976e748d636561b3
Tomasz <tomasz.golec.97@gmail.com>
2024-11-13 19:14:31 +0100

    finish player controls and add joystick

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -9,10 +9,14 @@ import 'package:flutter/material.dart';
 import 'package:pixel_adventure/actors/player.dart';
 import 'package:pixel_adventure/levels/level.dart';
 
-class PixelAdventure extends FlameGame with HasKeyboardHandlerComponents {
+class PixelAdventure extends FlameGame
+    with HasKeyboardHandlerComponents, DragCallbacks {
   @override
   Color backgroundColor() => const Color(0xFF211F30);
   Player player = Player(character: 'Mask Dude');
+  late JoystickComponent joystick;
+  bool showJoystick = false;
+
   late Level _world;
   late TiledComponent level;
   @override
@@ -25,5 +29,51 @@ class PixelAdventure extends FlameGame with HasKeyboardHandlerComponents {
     );
     camera.viewfinder.anchor = Anchor.topLeft;
     await add(_world);
+    if (showJoystick) {
+      addJoystick();
+    }
+  }
+
+  @override
+  void update(double dt) {
+    if (showJoystick) {
+      updateJoystick();
+    }
+    super.update(dt);
+  }
+
+  void addJoystick() {
+    joystick = JoystickComponent(
+      margin: const EdgeInsets.only(left: 32, bottom: 32),
+      knob: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Knob.png'),
+        ),
+      ),
+      background: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Joystick.png'),
+        ),
+      ),
+    );
+    add(joystick);
+  }
+
+  void updateJoystick() {
+    switch (joystick.direction) {
+      case JoystickDirection.left:
+      case JoystickDirection.upLeft:
+      case JoystickDirection.downLeft:
+        player.playerDirection = PlayerDirection.left;
+        break;
+      case JoystickDirection.right:
+      case JoystickDirection.upRight:
+      case JoystickDirection.downRight:
+        player.playerDirection = PlayerDirection.right;
+        break;
+      default:
+        player.playerDirection = PlayerDirection.none;
+        break;
+    }
   }
 }
